73fc551de9a846352848a141caf88615d911eba9
amineelakkermi <amineelakkermi37@gmail.com>
2025-11-09 22:20:13 +0100

    mise a jour

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -40,18 +40,18 @@ import zid from '../public/partners/zid.png';
 
 export default function DownloadButton() {
   return (
-    <div className="flex flex-wrap items-center gap-5 justify-center">
-     <a
-        href="https://zid.sa/"
+    <div className="flex flex-wrap items-center gap-5 justify-center"> 
+       <a
+        href="https://s.salla.sa/auth?intended_to=https%3A%2F%2Fs.salla.sa%2Fapps%2Finstall%2F1450881912%3Ftrial%3D1%26utm_source%3Dapps"
         target="_blank"
         rel="noreferrer noopener"
         className="flex items-center justify-center gap-3 w-48 h-16 rounded-full border border-white/15 bg-white/10 text-sm hover:bg-white/20 transition-colors"
       >
         تحميل عبر 
-      <Image src={zid} width={50} height={50} alt="Salla" className="logo object-contain" />
+      <Image src={partner4} width={50} height={50} alt="Salla" className="logo object-contain" />
 
       </a>
-     
+
        <a
         href="https://zid.sa/"
         target="_blank"
@@ -59,7 +59,7 @@ export default function DownloadButton() {
         className="flex items-center justify-center gap-3 w-48 h-16 rounded-full border border-white/15 bg-white/10 text-sm hover:bg-white/20 transition-colors"
       >
         تحميل عبر 
-      <Image src={partner4} width={50} height={50} alt="Salla" className="logo object-contain" />
+      <Image src={zid} width={50} height={50} alt="Salla" className="logo object-contain" />
 
       </a>
       
